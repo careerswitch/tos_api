c84413994549f6fb0efe67ee7afab621a28d0dc3
Ahmet Bolat <dev.ahmet.bolat@gmail.com>
2023-05-15 22:42:47 +0300

    - changed code to main()

--- get_token.py
+++ get_token.py
@@ -1,47 +1,58 @@
 #!/usr/bin/env python3
 
+"""
+
+code just for get_token
+
+"""
 import os
 import requests
 import sqlite3
 import time
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timedelta
 import pytz
 import json
 
-
-with open('config.json') as f:
-    config = json.load(f)
-
-client_id = config['client_id']
-refresh_token = config['refresh_token']
-
-DB_NAME = 'tokens.db'
+db_name = 'tokens.db'
 
 
 def create_database():
-    conn = sqlite3.connect(DB_NAME, check_same_thread=False)
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS tokens
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, token TEXT, timestamp INTEGER)''')
-    conn.commit()
-    conn.close()
+    print("Creating tokens database...")
+    try:
+        conn = sqlite3.connect(db_name, check_same_thread=False)
+        c = conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS tokens
+                     (timestamp INTEGER PRIMARY KEY AUTOINCREMENT, token TEXT)''')
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"Error creating database: {e}")
+        return None
 
 
 def insert_token(token):
-    conn = sqlite3.connect(DB_NAME)
+    conn = sqlite3.connect(db_name)
     c = conn.cursor()
     timestamp = int(datetime.now().timestamp())
     c.execute("INSERT INTO tokens (token, timestamp) VALUES (?, ?)", (token, timestamp))
     conn.commit()
     conn.close()
 
+    # Format the timestamp as a string
+    time_str = datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
+
+    print(f"New token created at {time_str}")
+
 
 def get_latest_token():
-    conn = sqlite3.connect(DB_NAME)
+    conn = sqlite3.connect(db_name)
     c = conn.cursor()
     c.execute("SELECT token FROM tokens ORDER BY timestamp DESC LIMIT 1")
     result = c.fetchone()
     conn.close()
+
+#    print("Check database for result of get_latest_token()")
+
     if result:
         return result[0]
     else:
@@ -59,35 +70,50 @@ def refresh_access_token(client_id, refresh_token):
         }
 
         response = requests.post(url, data=payload)
-
         access_token = response.json()['access_token']
 
         insert_token(access_token)
 
-        print(access_token)
-
-        # check if it's past 4pm EST time on a weekday
-        now = datetime.now(pytz.timezone('US/Eastern'))
-        if now.weekday() < 5 and now.hour >= 16:
-            break
-
         time.sleep(25 * 60)  # wait 25 minutes before requesting a new token
+        delete_old_tokens()  # delete any old tokens before getting the latest token
 
-    return get_latest_token()
+    return access_token
 
 
-def get_access_token():
-    if not os.path.isfile(DB_NAME):
+def delete_old_tokens():
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    timestamp_threshold = int((datetime.now() - timedelta(minutes=25)).timestamp())
+    c.execute("DELETE FROM tokens WHERE timestamp < ?", (timestamp_threshold,))
+    conn.commit()
+    conn.close()
+
+
+def main():
+    if not os.path.isfile(db_name):
         create_database()
 
-    latest_token = get_latest_token()
+    while True:
+        latest_token = get_latest_token()
 
-    if not latest_token:
-        latest_token = refresh_access_token(client_id, refresh_token)
-        print(latest_token)
-    else:
-        print(latest_token)
+        if not latest_token:
+            print("No latest token found in the database.")
+            with open('config.json') as f:
+                config = json.load(f)
 
-    return latest_token
+            client_id = config['client_id']
+            refresh_token = config['refresh_token']
+
+            delete_old_tokens()  # delete any old tokens before getting the latest token
+            latest_token = refresh_access_token(client_id, refresh_token)
+
+        else:
+            print("Check database for latest token:", latest_token[:5])
 
 
+        time.sleep(25 * 60)  # wait 25 minutes before requesting a new token and deleting old tokens
+        delete_old_tokens()  # delete any old tokens after 25 minutes have passed
+
+
+if __name__ == '__main__':
+    main()
